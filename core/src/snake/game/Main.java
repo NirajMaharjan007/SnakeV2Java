d264182887b013b6c5c1e8423ab45bd625c15c20
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-10-20 19:59:26 +0545

    updated

--- core/src/snake/game/Main.java
+++ core/src/snake/game/Main.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-// import com.badlogic.gdx.utils.ScreenUtils;
 import snake.game.asset.Asset;
 
 public class Main extends ApplicationAdapter {
