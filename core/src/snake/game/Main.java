37b951e9f302404ff2ef80962549e3430abad21a
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-10-20 17:54:39 +0545

    Added content

--- core/src/snake/game/Main.java
+++ core/src/snake/game/Main.java
@@ -1,31 +1,42 @@
 package snake.game;
 
+import snake.game.runnable.SnakeGame;
+
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+// import com.badlogic.gdx.utils.ScreenUtils;
+import snake.game.asset.Asset;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private SnakeGame game;
+
 	@Override
-	public void create () {
+	public void create() {
+		Asset.instance().loadAsset();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		game = new SnakeGame();
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+	public void render() {
+		game.update(Gdx.graphics.getDeltaTime());
+		clearScreen();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		game.render(batch);
 		batch.end();
 	}
-	
+
+	private void clearScreen() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		Asset.instance().dispose();
 	}
 }
